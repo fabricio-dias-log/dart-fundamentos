2c5f937ac88087c1787551c03ab759c4ce45b60f
Fabricio Dias <fabricio270909@gmail.com>
2025-02-06 23:10:48 -0300

    Variables by inference

---
+++ variaveis_02.dart
@@ -0,0 +1,10 @@
+void main() {
+  var a = 2;
+  var b = 4.56;
+  var c = 'Soma é: ';
+  print(c + (a + b).toString());
+
+  print(a.runtimeType);
+  print(b.runtimeType);
+  print(c.runtimeType);
+}
