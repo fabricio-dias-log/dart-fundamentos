2d40236fb80406230512f6ecf6b89d6958989ea6
Fabricio Dias <fabricio270909@gmail.com>
2025-02-07 22:48:34 -0300

    Unary Operators

---
+++ operadores_03.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Operadores Unários/Prefix
+  int a = 3;
+  int b = 4;
+
+  a++; // Postfix
+  --a; // Prefix
+
+  print(a);
+
+  print(a++ == --b);
+  print(a == b);
+  print(a);
+  print(b);
+
+  
+}
